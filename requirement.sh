2cea186ebfaf8e886b956e01bad7942f39822b4e
Ryan Chung <ryanccj@gmail.com>
2023-06-01 21:44:45 +0800

    renew NGS pipeline

---
+++ requirement.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PATH=$PATH:$(pwd)/toolkit/sratoolkit-3.0.5/bin
+export PATH=$PATH:$(pwd)/toolkit/bowtie-1.3.1
+export PATH=$PATH:$(pwd)/toolkit/bowtie2-2.5.1
+
+echo "Requirement Setting Done."
