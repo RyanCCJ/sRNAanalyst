e9959f910225e7eff2e450c8d946690de9141054
Ryan Chung <ryanccj@gmail.com>
2023-11-08 20:59:39 +0800

    delete pipeline

--- pipeline/22G_analyze.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# -------------------
-
-BASE_DIR=$(pwd)
-task=${BASE_DIR}/toolkit/sRNAanalyzer/src/analyze.py
-config_path=${BASE_DIR}/toolkit/sRNAanalyzer/config
-
-python $task --config $config_path --density --metagene --boundary --codon --fold --scatter
--- pipeline/example.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# add variables
-DATA=$1
-BASE_DIR=$(pwd)
-source $BASE_DIR/pipeline/example.conf
-
-# add command here...
-echo $text
