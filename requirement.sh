2dfe8fdafb3a33138b9a893ce7bd6118883ef0de
Ryan Chung <ryanccj@gmail.com>
2023-11-08 21:01:18 +0800

    delete script

--- requirement.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PATH=$(pwd)/toolkit:$PATH
-export PATH=$(pwd)/toolkit/FastQC:$PATH
-export PATH=$(pwd)/toolkit/sratoolkit-3.0.5/bin:$PATH
-export PATH=$(pwd)/toolkit/bowtie-1.3.1:$PATH
-export PATH=$(pwd)/toolkit/bowtie2-2.5.1:$PATH
-
-echo "Requirement Setting Done."
